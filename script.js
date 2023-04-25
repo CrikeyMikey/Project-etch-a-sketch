b82a55ff863a32c7610b54611cfb6cf4789bbf32
Aydan Hoover <hooveraydan@gmail.com>
2023-04-24 22:19:52 -0700

    Updated draw area colors, added drawing capability

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gridSize = 32
+const gridSize = 100
 const gridDiv = document.querySelector(".gridDiv")
 
 const createGrid = (gridSize) => {
@@ -9,13 +9,24 @@ const createGrid = (gridSize) => {
         for (let j = 0; j < gridSize; j++) {
             const widthAndHeight = 600 / gridSize;
             const gridBox = document.createElement('div')
+            
             gridBox.classList.add('grid-box')
             gridBox.style.width = `${widthAndHeight}px`
             gridBox.style.height = `${widthAndHeight}px`
+
+            gridBox.addEventListener('mouseenter', (event) => {
+                gridBox.style.backgroundColor = "gray";
+            })
             row.appendChild(gridBox)
         }
         gridDiv.appendChild(row);
     }
 }
+
+const resetGrid = () => {
+    const grid = document.querySelectorAll(".grid-box")
+    grid.style.backgroundColor = "black";
+}
+
 
 createGrid(gridSize);
