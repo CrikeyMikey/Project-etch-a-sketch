73cdfd2ed7e3bc33b07d2685b80060f9dc8fe3b7
Aydan Hoover <hooveraydan@gmail.com>
2023-04-25 17:49:28 -0700

    Functionality for board choice from user

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const gridSize = 100
+const gridSize = 12
 const gridDiv = document.querySelector(".gridDiv")
+const resetBtn = document.querySelector(".reset-btn")
+const container = document.querySelector(".gridDiv")
 
 const createGrid = (gridSize) => {
+    const wrapper = document.createElement('div')
+    wrapper.classList.add('wrapper')
+
+    //Generation loops for rows and boxes
     for (let i = 0; i < gridSize; i++) {
         const row = document.createElement('div')
         row.classList.add('grid-row')
@@ -9,24 +15,31 @@ const createGrid = (gridSize) => {
         for (let j = 0; j < gridSize; j++) {
             const widthAndHeight = 600 / gridSize;
             const gridBox = document.createElement('div')
-            
+    //Adds functionality for boxes to fill in when mouse enters them
             gridBox.classList.add('grid-box')
             gridBox.style.width = `${widthAndHeight}px`
             gridBox.style.height = `${widthAndHeight}px`
 
-            gridBox.addEventListener('mouseenter', (event) => {
-                gridBox.style.backgroundColor = "gray";
+            gridBox.addEventListener('mouseenter', () => {
+                gridBox.style.backgroundColor = 'gray';
             })
             row.appendChild(gridBox)
         }
-        gridDiv.appendChild(row);
+        wrapper.appendChild(row);
     }
+    container.appendChild(wrapper)
 }
 
-const resetGrid = () => {
-    const grid = document.querySelectorAll(".grid-box")
-    grid.style.backgroundColor = "black";
-}
+createGrid(gridSize)
 
+resetBtn.addEventListener("click", () => {
+    let userSize = Number(prompt('What size would you like the board to be?'))
 
-createGrid(gridSize);
+    while(userSize > 100) {
+        userSize = Number(prompt('Pick a smaller number, 100 or less'))
+    }
+
+    const wrapper = document.querySelector('.wrapper')
+    wrapper.remove()
+    createGrid(userSize);
+})
